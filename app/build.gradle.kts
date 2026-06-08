1c44336132a8c785838d16def2c4a23133c805b5
JanOkul <janokul92@gmail.com>
2026-06-08 23:26:51 +0100

    Added badge to the readme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,13 +26,15 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
             signingConfig = signingConfigs.getByName("debug")
         }
+
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
